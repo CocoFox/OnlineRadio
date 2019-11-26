a765fa62cabee3bb6dcfe92272641d672dd5fd9e
TaoVuHoai <tao.vuhoai@gmail.com>
2019-11-26 09:56:30 +0100

    commit client

--- server/server.js
+++ server/server.js
@@ -5,6 +5,17 @@ var server = express();
 var path = require('path');
 
 server.get('/radio', function(request, response) {
-    response.sendfile(path.resolve('./index.html'));
+    response.sendfile(path.resolve('./client/index.html'));
+   
+   
    });
+   
+   server.get('/json', function(request, response) {
+    const fs = require('fs')
+    let fichier = fs.readFileSync('./server/personne.json')
+    let personne = JSON.parse(fichier)
+    response.send(personne);    
+    
+   });
+
 server.listen(8080);
