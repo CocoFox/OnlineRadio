65083ca6b119128a5d31f2a6972879baff00aafc
Pacome Bondet <pabondet@gmail.com>
2019-12-02 09:40:21 +0100

    service without vlc

--- server/server.js
+++ server/server.js
@@ -6,21 +6,11 @@ var path = require('path');
 
 server.get('/radio', function (request, response) {
     response.sendfile(path.resolve('./client/index.html'));
-<<<<<<< HEAD
-
-
-});
-
-server.get('/json', function (request, response) {
-=======
-   
-   
    });
    
    server.get('/output_n', function(request, response) {
->>>>>>> a9584df7916a1158a1a49c2a877424e0c4c8462b
     const fs = require('fs')
-    let fichier = fs.readFileSync('./online_radio/output_n.json')
+    let fichier = fs.readFileSync('./server/output_n.json')
     let personne = JSON.parse(fichier)
     response.send(personne);
 
