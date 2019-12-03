44e8325deaafee879e7e803bf0026eabf7337f37
Pacome Bondet <pabondet@gmail.com>
2019-12-03 10:58:26 +0100

    vlc play

--- server/vlc/play_link.py
+++ server/vlc/play_link.py
@@ -1,5 +1,21 @@
 import vlc
+import sys
 
-player = vlc.MediaPlayer("http://start-adofm.ice.infomaniak.ch/start-adofm-high.mp3")
-
+link = "https://altacanzona.streamakaci.com/altacanzona.mp3"
+player = vlc.MediaPlayer("https://altacanzona.streamakaci.com/altacanzona.mp3")
+print(player)
 player.play()
+
+# if sys.argv[1] != 0:
+#     if sys.argv[2] != 0:
+#         player.play()
+#         print("play")
+# else:
+#     player.stop()
+ 
+
+    
+
+
+
+
