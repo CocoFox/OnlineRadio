934f0cb963b5be3edda1805ecce4c63dd3b86df0
Pacome Bondet <pabondet@gmail.com>
2019-12-03 11:17:04 +0100

    stream works

--- server/vlc/play_link.py
+++ server/vlc/play_link.py
@@ -2,18 +2,17 @@ import vlc
 import sys
 
 link = "https://altacanzona.streamakaci.com/altacanzona.mp3"
-player = vlc.MediaPlayer("https://altacanzona.streamakaci.com/altacanzona.mp3")
-print(player)
+player = vlc.MediaPlayer('https://altacanzona.streamakaci.com/altacanzona.mp3')
+print(player.play())
 player.play()
 
-# if sys.argv[1] != 0:
-#     if sys.argv[2] != 0:
-#         player.play()
-#         print("play")
-# else:
-#     player.stop()
- 
-
+while True:
+    if sys.argv[1] != 0:
+        if sys.argv[2] != 0:
+            player.play()
+            print("play")
+    else:
+        player.stop()
     
 
 
