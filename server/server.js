64dd522c7b7c82f749f98411e51a6241205474aa
TaoVuHoai <tao.vuhoai@gmail.com>
2019-12-10 09:58:41 +0100

    requete post

--- server/server.js
+++ server/server.js
@@ -3,22 +3,46 @@ var http = require('http');
 var express = require('express');
 var server = express();
 var path = require('path');
+let {PythonShell} = require('python-shell')
+var bodyParser = require('body-parser');
+const fs = require('fs');
+let fichier = fs.readFileSync('./server/output_n.json');
+let links = JSON.parse(fichier);
+var play = 0 ; 
 
 server.get('/radio', function (request, response) {
     response.sendfile(path.resolve('./client/index.html'));
    });
    
    server.get('/output_n', function(request, response) {
-    const fs = require('fs')
-    let fichier = fs.readFileSync('./server/output_n.json')
-    let personne = JSON.parse(fichier)
-    response.send(personne);
-
+    response.send(links);
 });
+  
+server.use(bodyParser.json() );
+server.use(bodyParser.urlencoded({extended: true }));
 
-server.get('/', function (request, response) {
-    response.sendFile(path.resolve('./server/radios_ubuntu.html'));
-});
+server.get('/run', runPy);
+server.post('/post',function (request,response){
+       //response.sendfile(path.resolve('./client/index.html'))
+       runPy(request.body.link,response);
+       console.log(request.body.link);  
 
+   })
+
+
+function runPy(link,res){
+        var options = {
+            args:
+            [
+              play=1, // starting funds
+              link, // wager count — number of wagers per sim
+            ]
+          }
+          PythonShell.run('.\\server\\vlc\\play_link.py', options, function (err, data) {
+        if (err){
+          res.send(err);
+          console.log(err);
+        }
+      })};
 
 server.listen(8080);
