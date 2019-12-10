7b15da5c268059ce2317196328d0ba37af4b3658
Pacome Bondet <pabondet@gmail.com>
2019-12-10 10:48:32 +0100

    recieve messages

--- server/vlc/play_link.py
+++ server/vlc/play_link.py
@@ -2,17 +2,18 @@ import vlc
 import sys
 
 link = "https://altacanzona.streamakaci.com/altacanzona.mp3"
-player = vlc.MediaPlayer('https://altacanzona.streamakaci.com/altacanzona.mp3')
-print(player.play())
-player.play()
+player = vlc.MediaPlayer()
 
 while True:
-    if sys.argv[1] != 0:
-        if sys.argv[2] != 0:
-            player.play()
-            print("play")
-    else:
-        player.stop()
+    for line in sys.stdin:
+        if 'stop':
+            player.stop()
+            break
+        else:
+            player.play(line)
+            break
+
+    
     
 
 
