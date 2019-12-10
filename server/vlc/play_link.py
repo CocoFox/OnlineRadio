79b803ea78db2b4406d2b490ffde1c46af89df6e
Pacome Bondet <pabondet@gmail.com>
2019-12-10 11:48:42 +0100

    receive calls

--- server/vlc/play_link.py
+++ server/vlc/play_link.py
@@ -2,16 +2,27 @@ import vlc
 import sys
 
 link = "https://altacanzona.streamakaci.com/altacanzona.mp3"
-player = vlc.MediaPlayer()
-
+player = vlc.MediaPlayer(link)
+# player.stop()
+print("vlc started")
+playback = False
 while True:
-    for line in sys.stdin:
-        if 'stop':
+    if playback:
+        player.play()
+    line = sys.stdin.readline()
+    if line:
+        
+        if 'stop' == line.rstrip('\n'):
             player.stop()
-            break
+            print("pystop")
+            line = ''
+            playback = False
         else:
-            player.play(line)
-            break
+            print("'" + line.rstrip('\n') + "'" )
+            player = vlc.MediaPlayer(line.rstrip('\n'))
+            print("pyplay")
+            line = ''
+            playback = True
 
     
     
